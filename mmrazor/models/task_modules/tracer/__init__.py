88b50db20cbc0a6e3c1209c48d388f014c364d74
pppppM <67539920+pppppM@users.noreply.github.com>
2022-12-01 12:21:43 +0800

    [Feature] Add `prepare_for_mmdeploy` interface  (#365)
    * remove useless code
    
    * fix build graph module import bug
    
    * refactor general quant
    
    * rename GeneralQuant to MMArchitectureQuant
    
    * fix some dtype bugs
    
    * add prepare_for_mmdeploy interface
    
    * update prepare for mmdeploy args
    
    * fix some comments
    
    Co-authored-by: humu789 <humu@pjlab.org.cn>

--- mmrazor/models/task_modules/tracer/__init__.py
+++ mmrazor/models/task_modules/tracer/__init__.py
@@ -2,8 +2,8 @@
 from .backward_tracer import BackwardTracer
 from .channel_analyzer import ChannelAnalyzer
 # from .razor_tracer import RazorFxTracer
-from .fx import (CustomTracer, UntracedMethodRegistry, custom_symbolic_trace,
-                 prepare_graph_module)
+from .fx import (CustomTracer, UntracedMethodRegistry, build_graphmodule,
+                 custom_symbolic_trace)
 from .loss_calculator import *  # noqa: F401,F403
 from .parsers import *  # noqa: F401,F403
 from .path import (Path, PathConcatNode, PathConvNode, PathDepthWiseConvNode,
@@ -14,5 +14,5 @@ __all__ = [
     'PathConcatNode', 'Path', 'PathList', 'PathNode', 'PathDepthWiseConvNode',
     'ChannelAnalyzer',
     'CustomTracer', 'UntracedMethodRegistry', 'custom_symbolic_trace',
-    'prepare_graph_module'
+    'build_graphmodule'
 ]
