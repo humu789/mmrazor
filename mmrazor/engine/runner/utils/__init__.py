1dc43eb82f3947854b4e92d6549adac119855e72
humu789 <88702197+humu789@users.noreply.github.com>
2022-12-29 14:30:37 +0800

    Quant go (#409)
    * add torch observer
    
    * add torch fakequant
    
    * refactor base quantizer
    
    * add QConfigHander and QSchemeHander & finish quantizer_refactor_beta
    
    * passed ptq_pipeline
    
    * tmp-commit
    
    * fix loop and algorithm
    
    * delete fakequant
    
    * refactor code structure
    
    * remove lsq
    
    * valid ptq pipeline
    
    * wip
    
    * fix del functions
    
    * fix
    
    * fix lint and pytest
    
    Co-authored-by: HIT-cwh <2892770585@qq.com>

--- mmrazor/engine/runner/utils/__init__.py
+++ mmrazor/engine/runner/utils/__init__.py
@@ -2,7 +2,5 @@
 from .calibrate_bn_mixin import CalibrateBNMixin
 from .check import check_subnet_resources
 from .genetic import crossover
-from .state import set_quant_state
-from .subgraph import extract_blocks, extract_layers, extract_subgraph
 
 __all__ = ['crossover', 'check_subnet_resources', 'CalibrateBNMixin']
